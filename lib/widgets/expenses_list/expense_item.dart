8bce70e29a033c75150059a036167104fedf1e05
Bhargav Pratim <bhargavsarma173@gmail.com>
2024-06-30 22:00:37 +0530

    design the item cards in of each expense item

--- lib/widgets/expenses_list/expense_item.dart
+++ lib/widgets/expenses_list/expense_item.dart
@@ -1,13 +1,39 @@
 import 'package:expense_tracker/models/expense_mod.dart';
 import 'package:flutter/material.dart';
 
-class ExpenseItem extends StatelessWidget{
+class ExpenseItem extends StatelessWidget {
   const ExpenseItem(this.expense, {super.key});
 
   final Expense expense;
-  
+
   @override
   Widget build(BuildContext context) {
-      return Card(child: Text(expense.title),);
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 20,
+          vertical: 60
+        ),
+        child: Column(
+          children: [
+            Text(expense.title),
+            const SizedBox(height: 4,),
+            Row(
+              children: [
+                Text('₹  ${expense.amount.toStringAsFixed(2)}'),
+                const Spacer(),
+                Row(
+                  children: [
+                    const Icon(Icons.water),
+                    const SizedBox(width: 8,),
+                    Text(expense.date.toString()),
+                  ],
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
