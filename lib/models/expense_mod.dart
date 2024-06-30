2519a1bcce6c58db02780030a32809f75e9fb03a
Bhargav Pratim <bhargavsarma173@gmail.com>
2024-06-30 20:05:18 +0530

    add expense category using enums

--- lib/models/expense_mod.dart
+++ lib/models/expense_mod.dart
@@ -2,11 +2,19 @@ import 'package:uuid/uuid.dart';
 
 const uuid = Uuid(); // for generating unique ids
 
+enum Category { food, travel, leisure, work }
+
 class Expense {
-  Expense({required this.title, required this.amount, required this.date}) : id = uuid.v4(); //initialiser list
+  Expense(
+      {required this.title,
+      required this.amount,
+      required this.date,
+      required this.category})
+      : id = uuid.v4(); //initialiser list
 
   final String id;
   final String title;
   final double amount;
   final DateTime date;
+  final Category category;
 }
