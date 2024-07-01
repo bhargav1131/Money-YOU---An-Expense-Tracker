e2de6f04e3d8c5f1f238ef92e51ffdfb0ec8b26a
Bhargav Pratim <bhargavsarma173@gmail.com>
2024-07-01 10:05:22 +0530

    remove the debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:expense_tracker/widgets/expenses.dart';
 void main(){
   runApp(
     const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Expenses(), // from file expenses.dart
     )
   );
