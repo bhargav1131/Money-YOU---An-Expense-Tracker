555719674d3f11467f611ad54be2bca10ae8fd13
Bhargav Pratim <bhargavsarma173@gmail.com>
2024-07-06 17:29:11 +0530

    add amount field, cancel button and functionality to save button

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -17,10 +17,12 @@ class _NewExpenseState extends State<NewExpense> {
   }*/
 
   final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
 
   @override
   void dispose() {
     _titleController.dispose();
+    _amountController.dispose();
     super.dispose();
   }
 
@@ -37,12 +39,27 @@ class _NewExpenseState extends State<NewExpense> {
               label: Text('Title'),
             ),
           ),
+          TextField(
+            controller: _amountController,
+            keyboardType: TextInputType.number,
+            decoration: const InputDecoration(
+              prefixText: '₹ ',
+              label: Text('Amount'),
+            ),
+          ),
           Row(
             children: [
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const Text('Cancel'),
+              ),
               ElevatedButton(
                 onPressed: () {
                   // print(_enteredTitle);
                   print(_titleController.text);
+                  print(_amountController.text);
                 },
                 child: const Text('Save Expense'),
               )
