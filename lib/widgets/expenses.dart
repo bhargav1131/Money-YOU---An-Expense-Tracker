4ea100150e4295c9cfeab7984f985a09005a7f6f
Bhargav Pratim <bhargavsarma173@gmail.com>
2024-07-08 21:19:37 +0530

    include functionality to show message and Undo the change while deleting an expense

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -48,9 +48,22 @@ class _ExpensesState extends State<Expenses> {
   }
 
   void _removeExpense(Expense expense){
+    final expenseIndex = _registeredExpenses.indexOf(expense);
     setState(() {
       _registeredExpenses.remove(expense);
     });
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        duration: const Duration(seconds: 3),
+        content: const Text('Expense deleted'),
+        action: SnackBarAction(label: 'Undo', 
+        onPressed: (){
+          setState(() {
+            _registeredExpenses.insert(expenseIndex, expense);
+          });
+        }),  
+      )
+    );
   }
 
   @override
