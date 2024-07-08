173e898b068467ed8244d8cccf30672e9bc0faa4
Bhargav Pratim <bhargavsarma173@gmail.com>
2024-07-08 20:20:12 +0530

    Adjust top padding

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -86,7 +86,7 @@ class _NewExpenseState extends State<NewExpense> {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(16),
+      padding: const EdgeInsets.fromLTRB(16, 45, 16, 16),
       child: Column(
         children: [
           TextField(
