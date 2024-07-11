cc0f2928c2915d65ff0705a2b9753f69c37d52a9
Bhargav Pratim <bhargavsarma173@gmail.com>
2024-07-11 14:07:48 +0530

    add an expense bucket class to create the chart

--- lib/models/expense_mod.dart
+++ lib/models/expense_mod.dart
@@ -5,27 +5,35 @@ import 'package:intl/intl.dart';
 const uuid = Uuid(); // for generating unique ids
 final formatter = DateFormat.yMd();
 
-enum Category { food, travel, leisure, work , healthcare, clothing, education, rent}
+enum Category {
+  food,
+  travel,
+  leisure,
+  work,
+  healthcare,
+  clothing,
+  education,
+  rent
+}
 
 const categoryIcons = {
-  Category.food : Icons.lunch_dining,
-  Category.travel : Icons.flight_takeoff,
-  Category.leisure : Icons.movie,
-  Category.work : Icons.work,
-  Category.healthcare : Icons.health_and_safety,
-  Category.clothing : Icons.collections_sharp,
-  Category.education : Icons.book,
-  Category.rent : Icons.home,
+  Category.food: Icons.lunch_dining,
+  Category.travel: Icons.flight_takeoff,
+  Category.leisure: Icons.movie,
+  Category.work: Icons.work,
+  Category.healthcare: Icons.health_and_safety,
+  Category.clothing: Icons.collections_sharp,
+  Category.education: Icons.book,
+  Category.rent: Icons.home,
 };
 
 class Expense {
   Expense(
-      {
-      required this.title,
+      {required this.title,
       required this.amount,
       required this.date,
-      required this.category
-      }): id = uuid.v4(); //initialiser list
+      required this.category})
+      : id = uuid.v4(); //initialiser list
 
   final String id;
   final String title;
@@ -33,7 +41,22 @@ class Expense {
   final DateTime date;
   final Category category;
 
-  String get formatttedDate{
+  String get formatttedDate {
     return formatter.format(date);
+  }
+}
+
+class ExpenseBucket {
+  const ExpenseBucket({required this.category, required this.expenses});
+
+  final Category category;
+  final List<Expense> expenses;
+
+  double get totalExpense {
+    double sum = 0;
+    for (final exp in expenses) {
+      sum = sum + exp.amount;
+    }
+    return sum;
   }
 }
